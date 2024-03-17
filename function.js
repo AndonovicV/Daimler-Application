47c9d730f7f9212b6fde7645c64269117ade907e
AndonovicV <andonovic.vojislav@gmail.com>
2024-03-17 10:52:20 +0100

    Bootstrap and jQuery import

--- function.js
+++ function.js
@@ -1,3 +1,24 @@
-function redFunction() {
-    document.getElementById("parag").style.color = "red";
-};
+//Check if Jquery is installed properly, will remove once it works for everyone.
+window.onload = function() {
+    if (window.jQuery) {  
+        // jQuery is loaded  
+        alert("Yeah! my jQuery is loaded!");
+    } else {
+        // jQuery is not loaded
+        alert("My jQuery Doesn't Work");
+    }
+}
+
+// Vanila JS:
+// function redFunction() {
+//     document.getElementById("blueParag").style.color = "red";
+// };
+
+//jQuery JS:
+$(document).ready(function(){
+    $("#redBtn").on("click", function(){
+        $("#blueParag").css("color", "red");
+        $("#blueParag").text("Now I'm red")
+    });
+});
+
