20a7abd2efb92f4f70c9ac5edc902033e7fd4e2b
AndonovicV <andonovic.vojislav@gmail.com>
2024-05-10 20:00:33 +0200

    Squashed commit of the following:
    commit 2bbd822165df490ca6911da541e737ee91a7e47c
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Mon May 6 12:22:34 2024 +0200
    
        Fix: new mt_agenda starts as the next biggest item_id
    
    commit eff2dcbd18fd2c72d9239cfb61355c469c766c4f
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Mon May 6 12:10:07 2024 +0200
    
        hide table before select
    
    commit 8d27d6f6010c33df487deeeb125f547cd2bd4540
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Mon May 6 11:58:53 2024 +0200
    
        Fix: new row linked to foreign key for mt_agenda
    
    commit 6e218c3f79a98abc814969e5fd9ae3484f2efd67
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Mon May 6 11:41:50 2024 +0200
    
        delete fix
    
    commit fba1a926160fe3bf78fd78eecc3dde6130894b8e
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Mon May 6 10:06:01 2024 +0200
    
        jquery local
    
    commit 5552e33ec541aee6ce1cc04af50e6dc67f722725
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Mon May 6 00:34:53 2024 +0200
    
        database upload
    
    commit cde136effec311a6bb34a126e9e38cced38f0bc1
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Mon May 6 00:29:37 2024 +0200
    
        test 2
    
    commit 67ca2f283f45bdf33b0fffead49054b244e3e969
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Mon May 6 00:21:33 2024 +0200
    
        MT_Agenda Link - draft
    
    commit 0ef612ff595893f0c5507864bb38df25015d41ef
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Sun May 5 21:51:20 2024 +0200
    
        changing mt agenda id to item_id
    
    commit c9060f8e80c1e58f0113ec3496789ca03fde8174
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Sun May 5 21:25:41 2024 +0200
    
        Squashed commit of the following:
    
        commit 6c6b2fbae71eb2c422c7661b8946b932db133f59
        Author: Aly Elhadad <54284629+Alybistata@users.noreply.github.com>
        Date:   Sun May 5 17:18:24 2024 +0200
    
            Voki fix now works both add and delete
    
    commit b59458cdf07cd1026335608767edf528e95686ad
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Sat Apr 27 12:31:16 2024 +0200
    
        changed structure
    
    commit 68358ad3861a95d934fc2906c838dbf0ef256452
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Sat Apr 27 11:51:26 2024 +0200
    
        cockpit_database_folder
    
    commit b5b16caa00b2907ff3f4c4cd227658aad2b20a61
    Author: Aly Elhadad <54284629+Alybistata@users.noreply.github.com>
    Date:   Thu Apr 25 18:30:51 2024 +0200
    
        Aly Datatable update - not final -
    
    commit 52b05074ef976d4c11fa4223ff48ed15a37427c1
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Thu Apr 25 15:32:37 2024 +0200
    
        Tried adding remove button (not working)
    
    commit bb6007783a0d46043192de0fcbcf48a36cd179b3
    Author: AndonovicV <andonovic.vojislav@gmail.com>
    Date:   Thu Apr 25 11:45:41 2024 +0200
    
        MT Agenda add new row works (datatable)

---
+++ actions.php
@@ -0,0 +1,103 @@
+<?php 
+include 'conn.php';  
+
+// mt_agenda new row
+if (isset($_POST['meanId']) && isset($_POST['counter']) && isset($_POST['agendaId'])) {
+    $meanId = $_POST['meanId'];
+    $counter = $_POST['counter'];
+    $agendaId = $_POST['agendaId']; // Get the agenda_id
+
+    $sql = "INSERT INTO mt_agenda (item_id, agenda_id, GFT, Topic, Status, Change_Request, Task, Comment, Milestone, Responsible, Start, New_Row, Delete_Row)
+            VALUES ( '$meanId', '$agendaId', '$counter', '$counter', '$counter', '$counter', '$counter', '$counter', '$counter', '$counter', '$counter', 'Yes', 'No')";
+    echo "SQL query: " . $sql; // Echo the SQL query
+    if ($conn->query($sql) === TRUE) {
+        echo "New record inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+} else {
+    //echo "Mean ID, Counter value, or Agenda ID is not set";
+}
+
+
+// mt_agenda delete row
+if (isset($_POST['rowId'])) {
+    $rowId = $_POST['rowId'];
+    //echo $rowId; // Corrected variable name
+    $sql = "DELETE FROM mt_agenda WHERE CONCAT(`mt_agenda`.`item_id`) = '$rowId'";
+    echo "SQL query: " . $sql; // Echo the SQL query
+    if ($conn->query($sql) === TRUE) {
+        //echo "Row deleted successfully.";
+    } else {
+        //echo "Error deleting row: " . $conn->error;
+    }
+} else {
+    //echo "RowId not provided.";
+}
+// fetching agenda data based on the selected agenda ID
+if (isset($_POST['agenda_id']) && $_POST['agenda_id'] != 'new') {
+    $agenda_id = $_POST['agenda_id'];
+    $sql = "SELECT * FROM mt_agenda WHERE agenda_id = $agenda_id";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            // Echo each row as HTML
+            echo "<tr id='" . $row["item_id"] . "'>"; 
+            echo "<td>" . $row["item_id"] . "</td>";
+            echo "<td>" . $row["GFT"] . "</td>";
+            echo "<td>" . $row["Topic"] . "</td>";
+            echo "<td>" . $row["Status"] . "</td>";
+            echo "<td>" . $row["Change_Request"] . "</td>";
+            echo "<td>" . $row["Task"] . "</td>";
+            echo "<td>" . $row["Comment"] . "</td>";
+            echo "<td>" . $row["Milestone"] . "</td>";
+            echo "<td>" . $row["Responsible"] . "</td>";
+            echo "<td>" . $row["Start"] . "</td>";
+            echo "<td><button class='btn btn-primary addRow'>New Row</button></td>";
+            echo "<td><button class='btn btn-danger deleteRow'>Delete Row</button></td>";
+            echo "</tr>";
+        }
+    } else {
+        // Handle case when no data is found
+        echo "<tr><td colspan='12'>No data found.</td></tr>";
+    }
+}
+
+// Insert new row (Table) into mt_agenda_list table
+if (isset($_POST['agenda_name']) && !empty($_POST['agenda_name'])) {
+    $agendaName = $_POST['agenda_name'];
+
+    // Fetch the maximum item_id from mt_agenda table
+    $maxItemIdQuery = "SELECT MAX(item_id) AS max_item_id FROM mt_agenda";
+    $maxItemIdResult = $conn->query($maxItemIdQuery);
+    if ($maxItemIdResult->num_rows > 0) {
+        $row = $maxItemIdResult->fetch_assoc();
+        $nextItemId = $row['max_item_id'] + 1;
+    } else {
+        $nextItemId = 1; // If no rows found, start from 1
+    }
+
+    // Insert new row into mt_agenda_list table
+    $insertSql = "INSERT INTO mt_agenda_list (agenda_name) VALUES ('$agendaName')";
+    if ($conn->query($insertSql) === TRUE) {
+        // Retrieve the auto-generated agenda_id
+        $agendaId = $conn->insert_id;
+
+        // Insert an empty row into mt_agenda table with the next item_id
+        $emptyRowSql = "INSERT INTO mt_agenda (item_id, agenda_id, GFT, Topic, Status, Change_Request, Task, Comment, Milestone, Responsible, Start, New_Row, Delete_Row) 
+                        VALUES ('$nextItemId', '$agendaId', '', '', '', '', '', '', '', '', '', 'Yes', 'No')";
+        if ($conn->query($emptyRowSql) === TRUE) {
+            echo $agendaId; // Return the newly generated agenda_id
+        } else {
+            echo "Error creating new agenda: " . $conn->error;
+        }
+    } else {
+        echo "Error creating new agenda: " . $conn->error;
+    }
+} else {
+    echo "Agenda name not provided.";
+}
+
+
+?>  
--- mt_agenda.php
+++ mt_agenda.php
@@ -1,64 +1,79 @@
-<table id="agendaTable" class="display">
-<thead>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011-04-25</td>
-                <td>$320,800</td>
-            </tr>
-            <tr>
-                <td>Garrett Winters</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>63</td>
-                <td>2011-07-25</td>
-                <td>$170,750</td>
-            </tr>
-            <tr>
-                <td>Ashton Cox</td>
-                <td>Junior Technical Author</td>
-                <td>San Francisco</td>
-                <td>66</td>
-                <td>2009-01-12</td>
-                <td>$86,000</td>
-            </tr>
-            <tr>
-                <td>Cedric Kelly</td>
-                <td>Senior Javascript Developer</td>
-                <td>Edinburgh</td>
-                <td>22</td>
-                <td>2012-03-29</td>
-                <td>$433,060</td>
-            </tr>
-            <tr>
-                <td>Airi Satou</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>33</td>
-                <td>2008-11-28</td>
-                <td>$162,700</td>
-            </tr>
-        </tbody>
-        <tfoot>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </tfoot>
-</table>
+<?php 
+include 'conn.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datatable</title>
+
+    <!-- Libraries -->
+    <script src="plugins\jQuery\jquery.min.js"></script>
+    <link href="plugins\bootstrap-5.3.3-dist\css\bootstrap.min.css" rel="stylesheet">
+    <script src="plugins\bootstrap-5.3.3-dist\js\bootstrap.min.js"></script>
+    <link rel="stylesheet" href="plugins\datatables\datatables.min.css">
+    <script src="plugins\datatables\datatables.min.js"></script>
+
+    <!-- Custom JS -->
+    <script src="custom_js/mt_agenda.js"></script>
+</head>
+
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Select or Create Agenda:</h3>
+            <select id="agendaSelect" class="form-select mb-3">
+                <option value="">Select Agenda...</option>
+                <?php
+                // Fetching data from mt_agenda_list table
+                $sql = "SELECT * FROM mt_agenda_list";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<option value='" . $row["agenda_id"] . "'>" . $row["agenda_name"] . "</option>";
+                    }
+                }
+                ?>
+                <option value="new">Create New Agenda</option>
+            </select>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <table id="agendaTable" class="display" style="display: none;">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>GFT</th>
+                        <th>Topic</th>
+                        <th>Status</th>
+                        <th>Change Request</th>
+                        <th>Task</th>
+                        <th>Comment</th>
+                        <th>Milestone</th>
+                        <th>Responsible</th>
+                        <th>Start</th>
+                        <th>New Row</th>
+                        <th>Delete Row</th>
+                    </tr>
+                </thead>
+                <tbody id="agendaTableBody">
+                    <!--Datatable body loaded here-->
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
+
+<?php
+$conn->close();
+?>
